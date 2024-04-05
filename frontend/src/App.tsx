de23e124e49bb1df5b663c7261c69d215fd0850d
Samuel Pires <samuel.ferpires@gmail.com>
2024-04-05 14:11:17 -0300

    FRONTEND: setup mantine ui and basic form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Container, Flex, Input, Textarea } from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container p={20} size={"50rem"}>
+      <Flex
+        direction={"column"}
+        gap={20}
+        justify={"space-between"}
+        align={"center"}
+      >
+        <Input
+          placeholder="Titulo"
+          style={{
+            width: "100%",
+          }}
+        />
+        <Textarea
+          style={{
+            width: "100%",
+          }}
+          placeholder="Descricao"
+          resize="vertical"
+          flex={1}
+        />
+        <Button fullWidth>Adicionar</Button>
+      </Flex>
+    </Container>
+  );
 }
 
-export default App
+export default App;
