81fe5ac38d3572986ef28d3c95a0150823fcdb1d
Samuel Pires <samuel.ferpires@gmail.com>
2024-04-05 11:46:58 -0300

    BACKEND: handling prisma errors

--- backend/modules/task/actions.ts
+++ backend/modules/task/actions.ts
@@ -2,11 +2,24 @@ import { db } from "../../database/prisma";
 import { task_schema_type } from "./dto";
 
 export const add_task = async (data: task_schema_type) => {
-  console.log(data);
   const result = await db.task.create({
     data,
   });
-  console.log(result);
+  return result.id;
+};
+
+export const find_and_edit_task = async (
+  id: string,
+  data: Partial<task_schema_type>
+) => {
+  const result = await db.task.update({
+    where: {
+      id,
+    },
+    data: {
+      ...data,
+    },
+  });
 
   return result.id;
 };
--- backend/modules/task/router.ts
+++ backend/modules/task/router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { db } from "../../database/prisma";
 import { task_schema } from "./dto";
-import { add_task } from "./actions";
+import { add_task, find_and_edit_task } from "./actions";
 import { ZodError } from "zod";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 const routes = Router();
 
@@ -16,7 +17,7 @@ routes.post("/", async (req, res) => {
     const valid_data = task_schema.parse(req.body);
     const created_id = await add_task(valid_data);
     return res.json({
-      message: `Task: ${created_id} created`,
+      created_id,
       success: true,
     });
   } catch (error) {
@@ -29,9 +30,48 @@ routes.post("/", async (req, res) => {
       });
     }
 
+    if (error instanceof PrismaClientKnownRequestError) {
+      return res.status(400).send({
+        success: false,
+        found_errors: error.meta,
+      });
+    }
+
     return res.status(500).send({
       success: false,
-      error,
+      error: "Unknown server error",
+    });
+  }
+});
+
+routes.put("/:id", async (req, res) => {
+  try {
+    const valid_data = task_schema.parse(req.body);
+    const edited_id = await find_and_edit_task(req.params.id, valid_data);
+    return res.json({
+      edited_id,
+      success: true,
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      console.log(JSON.stringify(error, null, 2));
+      const errors = error.issues.map((item) => item.message);
+      return res.status(400).send({
+        success: false,
+        found_errors: errors,
+      });
+    }
+
+    if (error instanceof PrismaClientKnownRequestError) {
+      return res.status(400).send({
+        success: false,
+        found_errors: error.meta,
+      });
+    }
+
+    return res.status(500).send({
+      success: false,
+      error: "Unknown server error",
     });
   }
 });
